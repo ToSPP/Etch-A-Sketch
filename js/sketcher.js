e6f2fdb439db608e50d731e7ac04e8699e179571
Maxim <esau@yandex.ru>
2017-09-26 16:54:35 +0300

    Add grid

---
+++ js/sketcher.js
@@ -0,0 +1,10 @@
+var size = 16;
+function fillField(size) {
+	for(var i = 1; i <= Math.pow(size, 2); i++) {
+		$('.gameField').append('<div class="pixel"></div>');
+		var pixelSize = 600 / size;
+		$('.pixel').css({'width': pixelSize, 'height': pixelSize});
+	}
+	
+}
+fillField(size);
